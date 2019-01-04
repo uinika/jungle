e6df192206fd1b76f859bdd29af9580c933f7487
hank.zheng <uinika@outlook.com>
2019-01-05 01:46:21 +0800

    Update

---
+++ README.md
@@ -0,0 +1,35 @@
+# 安装步骤
+
+## 建立虚拟环境
+
+```cmd
+C:\Workspace\django
+λ python -m venv venv
+```
+
+## 激活虚拟环境
+
+```cmd
+C:\Workspace\django
+λ .\venv\Scripts\activate.bat
+(venv) λ
+```
+
+## 安装项目依赖包
+
+```cmd
+pip install -r requirements.txt
+```
+
+## 启动 Django 服务
+
+```cmd
+(venv) λ python manage.py runserver 8080
+Performing system checks...
+
+System check identified no issues (0 silenced).
+January 03, 2019 - 21:31:48
+Django version 2.1.4, using settings 'mysite.settings'
+Starting development server at http://127.0.0.1:8080/
+Quit the server with CTRL-BREAK.
+```
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,22 @@
 from django.http import HttpResponse
 
+from .models import Question
+
+
 def index(request):
-    return HttpResponse("你好，这是一个投票应用！")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("你正在查看问题 %s 。" % question_id)
+
+
+def results(request, question_id):
+    response = "你看到的是问题 %s 的结果。"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("你正在对问题 %s 进行投票。" % question_id)
