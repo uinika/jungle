72dfe387e89ab3965e9a560bf4be6b409697db38
hank.zheng <uinika@outlook.com>
2019-01-05 17:27:44 +0800

    Update

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+c8budi)z&9*=z*iyd6s%f9q^vpkh(c(z^y-1(g!oz6v^w!tbf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -118,6 +118,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,18 @@
-from django.http import HttpResponse
-
+from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
+from django.http import Http404
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("你正在查看问题 %s 。" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
